6c056b4489bbaae3a5f574915512c456b87f80c8
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2021-04-14 02:23:41 -0400

    Access and creators ready

--- dataspiceExample.R
+++ dataspiceExample.R
@@ -5,13 +5,13 @@ create_spice()
 
 # Populate attributes
 
+F1 <-  list.files(path = getwd(), pattern = ".csv", full.names = T)
+
 for(i in 1:length(F1)){
   prep_attributes(data_path = F1[i])
 }
 edit_attributes()
-  # Populate access
-
-F1 <-  list.files(path = getwd(), pattern = ".csv", full.names = T)
+# Populate access
 
 for(i in 1:length(F1)){
   prep_access(data_path = F1[i])
