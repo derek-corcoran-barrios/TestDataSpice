53f4bb395b7c1707563dfd6b6009dac19fb03299
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2021-04-13 21:07:47 -0400

    More csv

---
+++ dataspiceExample.R
@@ -0,0 +1,21 @@
+library(dataspice)
+
+# Create metadata templates
+create_spice()
+
+# Populate attributes
+
+prep_attributes()
+edit_attributes()
+# Populate access
+
+prep_access()
+edit_access()
+
+# Edit creators
+
+edit_creators()  
+
+# biblio
+
+edit_biblio()
